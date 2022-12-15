30fde216fefd45ec9b9b5ec5f4dcc47473007817
Shreyash Wasnik <shreyashwasnik112@gmail.com>
2022-12-15 20:47:55 +0530

    UPLOAD: Accident Database

---
+++ Accident_DB.sql
@@ -0,0 +1,62 @@
+INSERT INTO ACCIDENT
+(Report_no,Date,Location)
+VALUES
+('M49415563','05/01/22','Ayodhya Nagar,Nagpur');
+INSERT INTO ACCIDENT
+(Report_no,Date,Location)
+VALUES
+('R16356265','22/04/20','Manish Nagar,Nagpur');
+INSERT INTO ACCIDENT
+(Report_no,Date,Location)
+
+VALUES
+('L45569852','17/09/21','Koregaon Park ,Pune');
+INSERT INTO ACCIDENT
+(Report_no,Date,Location)
+VALUES
+('A12536987','06/11/22','Navale Bridge,Pune');
+INSERT INTO ACCIDENT
+(Report_no,Date,Location)
+VALUES
+('J32568741','12/05/17','Itwari ,Nagpur');
+INSERT INTO ACCIDENT
+(Report_no,Date,Location)
+VALUES
+('U89564723','25/07/20','Sitabuldi, Nagpur');
+INSERT INTO ACCIDENT
+(Report_no,Date,Location)
+VALUES
+('C78625374','14/02/19','Andheri,Mumbai');
+INSERT INTO ACCIDENT
+(Report_no,Date,Location)
+VALUES
+('H12486397','09/12/16','Borivali,Mumbai');
+INSERT INTO ACCIDENT
+(Report_no,Date,Location)
+VALUES
+('F45632879','18/10/22','Navsaari,Amravati');
+INSERT INTO ACCIDENT
+(Report_no,Date,Location)
+VALUES
+('T56347892','07/05/21','Badnera Road,Amravati');
+INSERT INTO ACCIDENT
+(Report_no,Date,Location)
+VALUES
+('P45879339','16/06/19','Jagtap Nagar,Nashik');
+INSERT INTO ACCIDENT
+(Report_no,Date,Location)
+VALUES
+('K65781243','01/04/15','Dsouza Colony,Nashik');
+INSERT INTO ACCIDENT
+(Report_no,Date,Location)
+VALUES
+('D46986558','13/07/20','Madina Chowk,Nanded');
+INSERT INTO ACCIDENT
+(Report_no,Date,Location)
+VALUES
+('V78892632','09/03/21','Hingoli Gate,Nanded');
+INSERT INTO ACCIDENT
+(Report_no,Date,Location)
+VALUES
+('Y46327879','06/08/19','Dharampeth, Nagpur');
+SELECT * FROM ACCIDENT
