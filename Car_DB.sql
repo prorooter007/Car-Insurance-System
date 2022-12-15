0f452a8aa4138675a6a8ba3d7c20e06e46a79b3b
Shreyash Wasnik <shreyashwasnik112@gmail.com>
2022-12-15 20:48:26 +0530

    UPLOAD: Car Database

---
+++ Car_DB.sql
@@ -0,0 +1,61 @@
+INSERT INTO CAR
+(License, Model, Year)
+VALUES
+('MH-49-AG-3333', 'Maruti Alto', '2012');
+INSERT INTO CAR
+(License, Model, Year)
+VALUES
+('MH-30-SP-1212','Maruti WagonR', '2016');
+INSERT INTO CAR
+(License, Model, Year)
+VALUES
+('MH-49-P-8396', 'Honda City', '2015');
+INSERT INTO CAR
+(License, Model, Year)
+VALUES
+('MH-49-K-6633', 'Maruti Swift','2017');
+INSERT INTO CAR
+(License, Model, Year)
+VALUES
+('MH-31-OK-1235', 'Maruti 800', '2005');
+INSERT INTO CAR
+(License, Model, Year)
+VALUES
+('MH-31-D-7777', 'Hyundai i10', '2014');
+INSERT INTO CAR
+(License, Model, Year)
+VALUES
+('MH-40-PP-5544', 'Hyundai i20', '2019');
+INSERT INTO CAR
+ (License, Model, Year)
+VALUES
+('MH-14-QR-7845', 'Mahindra Scorpio', '2020');
+INSERT INTO CAR
+(License, Model, Year)
+VALUES
+('MH-31-PL-3698', 'Mahindra KUV100', '2018');
+INSERT INTO CAR
+(License, Model, Year)
+VALUES
+('MH-15-UI-6677', 'Maruti Ritz', '2013');
+INSERT INTO CAR
+(License, Model, Year)
+VALUES
+('MH-12-PK-5167', 'Maruti Ciaz', '2021');
+INSERT INTO CAR
+(License, Model, Year)
+VALUES
+('MH-33-D-6791', 'Renault Quid', '2022');
+INSERT INTO CAR
+(License, Model, Year)
+VALUES
+('MH-40-VM-2846', 'Renault Duster', '2017');
+INSERT INTO CAR
+(License, Model, Year)
+VALUES
+('MH-33-FF-3619', 'TATA Tiago', '2018');
+INSERT INTO CAR
+(License, Model, Year)
+VALUES
+('MH-21-FL-5527', 'TATA Indica', '2021');
+SELECT * FROM CAR
